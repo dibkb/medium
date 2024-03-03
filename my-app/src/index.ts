8ada566e774919e8feccc4b59da30d8c358b93aa
dibkb <dibas9110@gmail.com>
2024-03-03 19:12:28 +0530

    routing

--- my-app/src/index.ts
+++ my-app/src/index.ts
@@ -1,9 +1,25 @@
-import { Hono } from 'hono'
+import { Hono } from "hono";
+const app = new Hono();
+// GET
+app.get("/api/v1/blog/:id", (c) => {
+  const id = c.req.param("id");
+  return c.text(`Blog route with ${id}`);
+});
+app.get("/api/v1/blog/bulk", (c) => {
+  return c.text(`Blog bulk`);
+});
 
-const app = new Hono()
+// POST
+app.post("/api/v1/user/signup", (c) => {
+  return c.text(`Signup route`);
+});
+app.post("/api/v1/user/signin", (c) => {
+  return c.text(`Signin route`);
+});
+// PUT
+app.post("/api/v1/blog", (c) => {
+  return c.text(`Edit blog`);
+});
+// DELETE
 
-app.get('/', (c) => {
-  return c.text('Hello Hono!')
-})
-
-export default app
+export default app;
